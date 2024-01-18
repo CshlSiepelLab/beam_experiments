428c256c2e93269cf545781f4f1fef0cbe4ec080
StephenStaklinski <stephenstak@gmail.com>
2024-01-18 16:42:36 -0500

    add nexus script to test

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 #A bayesian approach to inferring anatomical tissue locations for internal nodes in a cancer phylogeny with labeled leaves.
 
+### Simulating data
+
 To simulate data from an activated simulate conda environment:
 ```
 ./scripts/sim_wrapper.sh --design RANDOM --out test_sim --sites 10 --mutrate 1.0 --samples 10 --migration migration_matrix/true_migration_prob_matrix.csv
 ```
 
 The output should provide all that is necessary as input to the inference method, which is the true tree and the tissue labels of the leaves. The barcode data is also provided in the form of a mutation matrix.
+
+### Formatting simulated data to input to BEAST2
+
+Use simulate environment and input tree newick file and tsv of node to tissue mapping from simulated data:
+```
+python ./scripts/format_nexus_from_simulated_data.py simulated_data/sim_results_test_sim/test_sim_true.nwk simulated_data/sim_results_test_sim/test_sim_tissues.tsv
+```
---
+++ scripts/format_nexus_from_simulated_data.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import ete3
+import pandas as pd
+
+def create_nexus_from_dict(data_dict, site_number):
+    nexus_content = "#NEXUS\nBEGIN DATA;\n"
+    
+    # Determine the number of taxa
+    taxa_count = len(data_dict)
+    
+    # Write the dimensions and format
+    nexus_content += f"DIMENSIONS NTAX={taxa_count} NCHAR={site_number};\n"
+    nexus_content += "FORMAT DATATYPE=STANDARD MISSING=? GAP=-;\n"
+    
+    # Write the matrix
+    nexus_content += "MATRIX\n"
+    
+    for taxon_id, state in data_dict.items():
+        # Use taxon_id as the taxon label
+        nexus_content += f"{taxon_id} {state}\n"
+    
+    nexus_content += ";\n"
+    nexus_content += "END;\n"
+    
+    return nexus_content
+
+# newick_filepath = str(sys.argv[1])
+# tissue_data_filepath = str(sys.argv[2])
+newick_filepath = "simulated_data/sim_results_test_sim/test_sim_true.nwk"
+tissue_data_filepath = "simulated_data/sim_results_test_sim/test_sim_tissues.tsv"
+
+tree = ete3.Tree(newick_filepath)
+tissues_df = pd.read_csv(tissue_data_filepath, sep="\t")
+
+leaf_names = [int(leaf.name) for leaf in tree.iter_leaves()]
+
+leaf_tissues_df = tissues_df[tissues_df['node'].isin(leaf_names)]
+
+leaf_tissues_dict = dict(zip(leaf_tissues_df['node'], leaf_tissues_df['tissue']))
+
+# Make nexus file for a single site of tissue label aligned across samples
+nexus_content = create_nexus_from_dict(leaf_tissues_dict, 1)
+
+file_path = newick_filepath.split(".")[0] + ".nex"
+with open(file_path, "w") as nexus_file:
+    nexus_file.write(nexus_content)
