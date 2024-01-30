936204a18acd1ad58679713fdd80c6b7bc2e4fa0
Stephen Staklinski <109625525+StephenStaklinski@users.noreply.github.com>
2024-01-30 14:46:59 -0500

    upate collapse node script

--- scripts/collapse_tree_nodes_low_posterior_support.py
+++ scripts/collapse_tree_nodes_low_posterior_support.py
@@ -13,7 +13,12 @@ def collapse_low_support_nodes(tree, threshold):
         j = j + 1
         posterior = node.annotations.get_value('posterior')
         if posterior is not None and float(posterior) < threshold:
-            node.collapse_clade()
+            parent = node.parent_node
+            branch_length_remove = node.edge_length
+            for child in node.child_node_iter():
+                child.edge_length += branch_length_remove
+                parent.add_child(child)
+            parent.remove_child(node)
             i = i + 1
     print(f"Collapsed {i} out of {j} internal nodes with low posterior support.")
 
@@ -21,12 +26,12 @@ nexus_tree_file_path = sys.argv[1]
 threshold = float(sys.argv[2])
 
 #nexus_tree_file_path = "/Users/staklins/projects/crispr-barcode-cancer-metastasis/bayesian_phylogenetic_metastasis/examples/real_data/mmus1495/results/cp01/cp01_mutation_matrix_reformatted_tidetree_tidetree_sequences_formatted_for_tidetree.1706295631518.tree"
+#threshold = 0.5
 
 
 tree = dendropy.Tree.get(path=nexus_tree_file_path, schema='nexus')
 print("Input tree topology:")
 print(tree.as_ascii_plot())
-#collapse_low_support_nodes(tree, 0.5)
 collapse_low_support_nodes(tree, threshold)
 
 # Print the modified tree
@@ -34,4 +39,4 @@ print("Collapsed tree topology:")
 print(tree.as_ascii_plot())
 #print(tree.as_string('newick'))
 output_nexus_file_path = ".".join(nexus_tree_file_path.split(".")[:-1]) + f"_collapsed_nodes.tree"
-tree.write_to_path(output_nexus_file_path, schema='nexus')
+tree.write(path=output_nexus_file_path, schema='nexus')
