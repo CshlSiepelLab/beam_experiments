896d9853e145fe855e3443c056c53522bda36ae6
Staklinski <staklins@evolgen.cshl.edu>
2024-02-16 11:03:25 -0500

    add figtree command line script

--- scripts/figtree_plot_tree.sh
+++ scripts/figtree_plot_tree.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 treefile=$1
+#figtree_template=$2
+### Below works with script called from home directory to avoid having to always input figtree template path
+figtree_template="./inputs/template_figtree_block.tree"
 
-figtree="/home/staklins/bin/FigTree_v1.4.4/lib/figtree.jar"
+#figtree="/home/staklins/bin/FigTree_v1.4.4/lib/figtree.jar"
+figtree="$(which figtree.jar)"
 
+# combine figtree template with tree file
+figtree_treefile="${treefile/.tree/_figtree.tree}"
+outfile="${treefile/.tree/_figtree.pdf}"
+cat $treefile $figtree_template > $figtree_treefile
 
-
-$figtree -graphic PDF $treefile
+java -jar $figtree -graphic PDF $figtree_treefile $outfile
