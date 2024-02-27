9b3099e401d18337e064a9c0c11ddb6dc832ef6d
Staklinski <staklins@evolgen.cshl.edu>
2024-02-27 17:44:04 -0500

    add machina sim fas files

--- scripts/machina_reads_to_clone_alignment.py
+++ scripts/machina_reads_to_clone_alignment.py
@@ -5,11 +5,27 @@
 import sys
 import pandas as pd
 
-# reads_tsv=sys.argv[1]
+reads_tsv=sys.argv[1]
 
-reads_tsv="machina_m5_sim_data/seed0/reads_seed0.tsv"
+# reads_tsv="machina_data/sims/machina_m5_sim_data/seed0/reads_seed0.tsv"
 
 colnames = ["sample_index", "sample_label", "anatomical_site_index", "anatomical_site_label", "character_index", "character_label", "ref", "var"]
 reads_df = pd.read_csv(reads_tsv, sep='\t', names=colnames, skiprows=4)
 
-reads_df
+cols = ["sample_index", "var"]
+reads_df_subset = reads_df.loc[:, cols]
+grouped_df = reads_df_subset.groupby("sample_index")
+
+alignment = ""
+for index, group in grouped_df:
+    alignment += f">Clo{index}\n"
+    for row in group.iterrows():
+        if int(row[1]["var"]) == 0:
+            alignment += "T"
+        else:
+            alignment += "A"
+    alignment += "\n"
+
+outfile = reads_tsv.split(".")[0] + ".fas"
+with open(outfile, "w") as file:
+    file.write(alignment)
