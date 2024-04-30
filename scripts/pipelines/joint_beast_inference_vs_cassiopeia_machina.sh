71e8466806127f48236e75be2aae507f3848ade9
Staklinski <staklins@evolgen.cshl.edu>
2024-04-30 11:07:23 -0400

    update env specs

--- scripts/pipelines/joint_beast_inference_vs_cassiopeia_machina.sh
+++ scripts/pipelines/joint_beast_inference_vs_cassiopeia_machina.sh
@@ -31,35 +31,36 @@ metastabayes_jar="../metastabayes/metastabayes.jar"
 # get working dir
 dir=$(dirname "$sim_matrix")
 
-# run cassiopeia-greedy on matrix
-conda activate simulate
-python scripts/cassiopeia_greedy.py $sim_matrix
+# # run cassiopeia-greedy on matrix
+# conda activate simulate
+# python scripts/cassiopeia_greedy.py $sim_matrix
 
-# run machina on cassiopeia-greedy inferred tree
-# Prep cas tree for MACHINA input files
-cas_tree_tissues="${dir}/cassiopeia_greedy_inferred.nwk"
-machina_dir="${dir}/machina"
-primary_tissue="P"
-mkdir ${machina_dir}
-python ./scripts/machina/prep_machina.py ${cas_tree_tissues} ${machina_dir} ${primary_tissue} ${leaf_tissues}
-conda deactivate
+# # run machina on cassiopeia-greedy inferred tree
+# # Prep cas tree for MACHINA input files
+# cas_tree_tissues="${dir}/cassiopeia_greedy_inferred.nwk"
+# machina_dir="${dir}/machina"
+# primary_tissue="P"
+# mkdir ${machina_dir}
+# python ./scripts/machina/prep_machina.py ${cas_tree_tissues} ${machina_dir} ${primary_tissue} ${leaf_tissues}
+# conda deactivate
 
-# Run MACHINA
-conda activate machina
-#sed -i '0,/0/s/0/GL/' ${machina_dir}/*.tree     # Prevents MACHINA segmentation fault due to input formatting
-./scripts/machina/run_machina_tr.sh --edges ${machina_dir}/*.tree --labels ${machina_dir}/*.labeling --colors ${machina_dir}/*_colors.txt --primary-tissue ${primary_tissue} --outdir ${machina_dir}
-conda deactivate
+# # Run MACHINA
+# conda activate machina
+# #sed -i '0,/0/s/0/GL/' ${machina_dir}/*.tree     # Prevents MACHINA segmentation fault due to input formatting
+# ./scripts/machina/run_machina_tr.sh --edges ${machina_dir}/*.tree --labels ${machina_dir}/*.labeling --colors ${machina_dir}/*_colors.txt --primary-tissue ${primary_tissue} --outdir ${machina_dir}
+# conda deactivate
 
-# Condense MACHINA output into a labeled tree newick format
-conda activate simulate
-python ./scripts/machina/post_machina_tr_to_tree.py ${machina_dir}/P-T-P-R.tree ${machina_dir}/P-T-P-R.labeling ${machina_dir}
-conda deactivate
-# Remove intermediate MACHINA output files
-machina_tree="${dir}/machina_tree_all_tissue_labels.nwk" 
-mv ${machina_dir}/machina_tree_all_tissue_labels.nwk ${machina_tree}
-#rm -r ${machina_dir}
+# # Condense MACHINA output into a labeled tree newick format
+# conda activate simulate
+# python ./scripts/machina/post_machina_tr_to_tree.py ${machina_dir}/P-T-P-R.tree ${machina_dir}/P-T-P-R.labeling ${machina_dir}
+# conda deactivate
+# # Remove intermediate MACHINA output files
+# machina_tree="${dir}/machina_tree_all_tissue_labels.nwk" 
+# mv ${machina_dir}/machina_tree_all_tissue_labels.nwk ${machina_tree}
+# #rm -r ${machina_dir}
 
 # setup joint beast inference
+conda activate compare_trees
 template_xml="inputs/joint_inference_beast_template.xml"
 sim_time=$(grep "Number of generations" $drivers | cut -d':' -f2 | tr -d ' ')
 scripts/format_joint_inference_beast_xml.sh ${sim_matrix} ${leaf_tissues} ${template_xml} ${sim_time}
@@ -71,7 +72,6 @@ mcc_tree="${dir}/joint_inference_beast_tissues.tree"
 ${treeannotator_path} -burnin 10 -topology MCC -height mean -file ${beast_posterior_trees} ${mcc_tree}
 
 # get tissue labeled true tree
-conda activate compare_trees
 python scripts/format_add_tissues_to_newick.py ${true_tree} ${true_tissues}
 true_tissue_tree=${dir}/*_tissue_labeled_tree.nwk
 
