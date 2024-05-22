b955a874a28cc2538fca8ffa9818f229bf6b3dee
StephenStaklinski <stephenstak@gmail.com>
2024-05-22 14:54:37 -0400

    add new script

---
+++ scripts/newick_to_nexus.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+from Bio import Phylo
+
+# # user input 
+# newick_file = sys.argv[1]
+
+# testing
+newick_file="seed0.nwk"
+
+# format outfile
+outfile=newick_file.replace(".nwk",".tree")
+
+# read in newick tree
+tree = Phylo.read(newick_file, 'newick')
+
+# write out nexus tree
+Phylo.write(tree, outfile, 'nexus')
