b99f7e23cb0990a1ef826d307fbe919d62158ac6
StephenStaklinski <stephenstak@gmail.com>
2024-06-07 17:33:53 -0400

    move to a submission style for larger parallel jobs

--- scripts/pipelines/parallel_joint_beast_inference_vs_cassiopeia_machina.sh
+++ scripts/pipelines/parallel_joint_beast_inference_vs_cassiopeia_machina.sh
@@ -18,7 +18,6 @@ accuracy_file="${pipeline_run_name}/individual_joint_inference_accuracy.csv"
 echo "dir_name,machina_f1,beast_mcc_f1,beast_posterior_f1,random_f1,consensus_f1,beast_95ci_binary_downsampled_mg,true_mg_machina_f1,true_mg_beast_mcc_f1,true_mg_beast_posterior_f1,true_mg_random_f1,true_mg_consensus_f1,beast_95ci_binary_true_mg,ess_convergence,migration_count,cas_rf_dist,joint_rf_dist,shannon_mut_matrix" > $accuracy_file
 
 commands=()
-
 for dir in ${pipeline_run_name}/*/*;
 do
 # cmd="scripts/pipelines/joint_beast_inference_vs_cassiopeia_machina.sh $dir $accuracy_file"
@@ -26,11 +25,19 @@ cmd="scripts/pipelines/joint_beast_inference_vs_cassiopeia_machina.sh $dir $accu
 commands+=("$cmd")
 done
 
+i=0
 for command in "${commands[@]}"
 do
-  echo "${command}" >> "${pipeline_run_name}/parallel.txt"
+  # echo "${command}" >> "${pipeline_run_name}/parallel.txt"
+
+  echo "#!/bin/bash" > "${pipeline_run_name}/parallel${i}.sh"
+  echo "${command}" >> "${pipeline_run_name}/parallel${i}.sh"
+  chmod u+x "${pipeline_run_name}/parallel${i}.sh"
+  qsub -cwd -l m_mem_free=5G -pe threads 5 "${pipeline_run_name}/parallel${i}.sh"
+  i=$((i+1))
 done
 
-threads=4
-parallel -j 20 < "${pipeline_run_name}/parallel.txt"
-rm "${pipeline_run_name}/parallel.txt"
+# parallel -j 29 < "${pipeline_run_name}/parallel.txt"
+# rm "${pipeline_run_name}/parallel.txt"
+
+rm "${pipeline_run_name}/parallel*.sh"
