b9491d3e59b26021a3e8b0eb90161c2fc4f1a4a1
StephenStaklinski <stephenstak@gmail.com>
2024-10-02 11:19:52 -0400

    fix formatting bug in prepMetastabayesInputs

--- scripts/pipelines/snakemake_performance_yang_data/submit.sh
+++ scripts/pipelines/snakemake_performance_yang_data/submit.sh
@@ -3,7 +3,7 @@
 export REPO_PATH=/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/scripts/pipelines/snakemake_performance_yang_data
 
 snakemake \
---dry-run \
+--until prepMetastabayesInputs \
 --use-conda \
 --use-singularity \
 --singularity-args "--bind $HOME/" \
