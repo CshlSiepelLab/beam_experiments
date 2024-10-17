6ef71b73aee93c3aaaa4c012f410d853bc258f54
StephenStaklinski <stephenstak@gmail.com>
2024-10-17 10:25:01 -0400

    remove testing lines

--- scripts/consensus_random/consensus_random_parsimony_tissue_trees.py
+++ scripts/consensus_random/consensus_random_parsimony_tissue_trees.py
@@ -117,9 +117,9 @@ tree_file = sys.argv[1]
 leaf_tissues_tsv = sys.argv[2]
 outdir = sys.argv[3]
 
-tree_file = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/snakemake_performance_repeat_origin_scaling_implemented_10_15_24_uniform_50cells_50sites_data_7_24_24/laml/mS_854/mS_854_laml_trees_no_branch_lengths.nwk"
-leaf_tissues_tsv = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/snakemake_performance_repeat_origin_scaling_implemented_10_15_24_uniform_50cells_50sites_data_7_24_24/raw_data/mS_854/cell_tree_seed1833437564.labeling"
-outdir = "."
+# tree_file = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/snakemake_performance_repeat_origin_scaling_implemented_10_15_24_uniform_50cells_50sites_data_7_24_24/laml/mS_854/mS_854_laml_trees_no_branch_lengths.nwk"
+# leaf_tissues_tsv = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/snakemake_performance_repeat_origin_scaling_implemented_10_15_24_uniform_50cells_50sites_data_7_24_24/raw_data/mS_854/cell_tree_seed1833437564.labeling"
+# outdir = "."
 
 
 tree = ete3.Tree(tree_file, format=8)
