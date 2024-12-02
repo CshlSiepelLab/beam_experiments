08c0225aa8d3a4459eaf1979650c79a4facca048
StephenStaklinski <stephenstak@gmail.com>
2024-12-02 18:00:57 -0500

    add model selection xml files for mmus1467 cp01 runs

--- scripts/plotting/plot_rate_matrix_from_beast_log.py
+++ scripts/plotting/plot_rate_matrix_from_beast_log.py
@@ -23,12 +23,23 @@ tissues = [primary_tissue] + sorted([tis for tis in tissues if tis != primary_ti
 
 # Calculate average rates
 rates = {}
+# total_sum = 0  
 
 for source in tissues:
+    sum_rates = 0
     for recipient in tissues:
         if source == recipient:
             continue
         rates[f"{source}_{recipient}"] = df[f'geoSubstModelLogger.relGeoRate_{source}_{recipient}'].mean()
+
+#         sum_rates += rates[f"{source}_{recipient}"]
+    
+#     rates[f"{source}_{source}"] = -sum_rates
+#     total_sum += sum_rates
+
+# # Normalize rates by dividing by total_sum
+# for key in rates:
+#     rates[key] /= total_sum
 
 # Create a matrix for the rates
 num_tissues = len(tissues)
@@ -44,7 +55,7 @@ for (source_recipient), rate in rates.items():
 fs = 28
 ts = 18
 plt.figure(figsize=(10, 8))
-heatmap = sns.heatmap(rate_matrix, xticklabels=tissues, yticklabels=tissues, annot=True, cmap='YlOrRd', annot_kws={"size": ts}, cbar_kws={'label': 'Relative units'})
+heatmap = sns.heatmap(rate_matrix, xticklabels=tissues, yticklabels=tissues, annot=True, cmap='YlOrRd', annot_kws={"size": ts}, cbar_kws={'label': 'Rate'})
 heatmap.figure.axes[-1].yaxis.label.set_size(fs)
 heatmap.figure.axes[-1].tick_params(labelsize=ts)
 plt.xlabel('Recipient', fontsize=fs)
@@ -56,3 +67,25 @@ plt.tight_layout()
 plt.savefig(outfile)
 
 plt.close()
+
+# plot the distribution histogram for geoSubstModelLogger.relGeoRate columns for those rates with the recipient as primary_tissue
+primary_tissue_rates = [df[f'geoSubstModelLogger.relGeoRate_{source}_{primary_tissue}'] for source in tissues if source != primary_tissue]
+
+for source in tissues:
+    if source == primary_tissue:
+        continue
+    primary_tissue_rate = df[f'geoSubstModelLogger.relGeoRate_{source}_{primary_tissue}']
+
+    plt.figure(figsize=(8, 6))
+    sns.histplot(primary_tissue_rate, bins=30, kde=False, color='grey')
+    plt.xlabel('Rate', fontsize=fs)
+    plt.ylabel('Frequency', fontsize=fs)
+    plt.title(f'{source} to {primary_tissue}', fontsize=fs)
+    plt.xticks(fontsize=ts)
+    plt.yticks(fontsize=ts)
+    plt.tight_layout()
+
+    histogram_outfile = input_file.replace(".log", f"_{source}_to_{primary_tissue}_rate_distribution.pdf")
+    plt.savefig(histogram_outfile)
+
+    plt.close()
---
+++ scripts/plotting/plot_rate_matrix_setup_structure.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import re
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import BoundaryNorm
+import matplotlib.pyplot as plt
+
+# input_file = sys.argv[1]
+# primary_tissue = sys.argv[2]
+
+input_file = '/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/serio_prostate_cancer_data_11_18_24_asv_cutoff_50/beam/MMUS1467/CP01/combined.log'
+primary_tissue = 'PRL'
+
+outfile = input_file.replace(".log", "_tissue_rate_matrix_hypothesis_setups.pdf")
+
+df = pd.read_csv(input_file, sep='\t', comment='#')
+
+tissue_rate_col_names = [name for name in df.columns if name.startswith('geoSubstModelLogger')]
+tissues = list(set([tissue for name in tissue_rate_col_names for tissue in name.replace("geoSubstModelLogger.relGeoRate_", "").split("_") ]))
+tissues = [primary_tissue] + sorted([tis for tis in tissues if tis != primary_tissue])
+
+# Create a matrix for the rates with labels
+num_tissues = len(tissues)
+rate_matrix_labels = np.empty((num_tissues, num_tissues), dtype=object)
+rate_matrix_primary_zero = np.empty((num_tissues, num_tissues), dtype=object)
+
+rate_counter_continuous = 1
+rate_counter_zero = 1
+
+for i, source in enumerate(tissues):
+    for j, recipient in enumerate(tissues):
+        if source == recipient:
+            rate_matrix_labels[i, j] = -1
+            rate_matrix_primary_zero[i, j] = -1
+        else:
+            if recipient == primary_tissue:
+                rate_matrix_primary_zero[i, j] = 0
+                rate_matrix_labels[i, j] = 10   # set specific to 4x4 matrix
+            else:
+                rate_matrix_primary_zero[i, j] = rate_counter_zero
+                rate_counter_zero += 1
+                rate_matrix_labels[i, j] = rate_counter_continuous
+                rate_counter_continuous += 1
+
+rate_matrix_labels = rate_matrix_labels.astype(int)
+rate_matrix_primary_zero = rate_matrix_primary_zero.astype(int)
+
+# Plot the rate matrices
+fs = 28
+fig, axes = plt.subplots(1, 2, figsize=(20, 8))
+
+
+colors = ['grey', 'white'] + ['grey'] * 9
+cmap_null = ListedColormap(colors)
+
+colors = ['grey', 'white'] + ['grey'] * 9 + ['orange']
+cmap_alt = ListedColormap(colors)
+
+
+sns.heatmap(rate_matrix_primary_zero, xticklabels=tissues, yticklabels=tissues, annot=True, fmt='', cmap=cmap_null, cbar=False, ax=axes[0], annot_kws={"size": fs})
+axes[0].set_title('H$_{null}$', fontsize=fs)
+axes[0].set_xlabel('Recipient', fontsize=fs)
+axes[0].set_ylabel('Source', fontsize=fs)
+axes[0].tick_params(labelsize=fs)
+
+sns.heatmap(rate_matrix_labels, xticklabels=tissues, yticklabels=tissues, annot=True, fmt='', cmap=cmap_alt, cbar=False,  ax=axes[1], annot_kws={"size": fs})
+axes[1].set_title('H$_{alt}$', fontsize=fs)
+axes[1].set_xlabel('Recipient', fontsize=fs)
+axes[1].set_ylabel('Source', fontsize=fs)
+axes[1].tick_params(labelsize=fs)
+
+plt.tight_layout()
+plt.savefig(outfile)
+plt.close()
