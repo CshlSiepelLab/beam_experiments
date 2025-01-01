d5b1db3147f30f3d633b19b37a9f06d13785fdc8
StephenStaklinski <stephenstak@gmail.com>
2025-01-01 08:14:37 -0500

    Update

--- scripts/pipelines/snakemake_performance/submit.sh
+++ scripts/pipelines/snakemake_performance/submit.sh
@@ -3,7 +3,7 @@
 export REPO_PATH=/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/scripts/pipelines/snakemake_performance
 
 snakemake \
---until plotTrueMigrationGraph \
+--until plotGraphPosterior \
 --use-singularity \
 --singularity-args "--bind $HOME/" \
 --latency-wait 300 \
