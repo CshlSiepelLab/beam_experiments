376fcef82f78c5b18d217500a3877e6959dfb310
StephenStaklinski <stephenstak@gmail.com>
2025-01-04 10:27:52 -0500

    Update pipelines

--- scripts/pipelines/snakemake_performance/submit.sh
+++ scripts/pipelines/snakemake_performance/submit.sh
@@ -3,7 +3,6 @@
 export REPO_PATH=/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/scripts/pipelines/snakemake_performance
 
 snakemake \
---until runBeam \
 --use-singularity \
 --singularity-args "--bind $HOME/" \
 --latency-wait 300 \
