cd9980e389fada6ed0147a6466c65b46078f2e77
StephenStaklinski <stephenstak@gmail.com>
2025-01-07 10:40:14 -0500

    Add script to sample posterior trees to get their metastasis event times

--- scripts/formatting/sample_metastasis_times_from_posterior.py
+++ scripts/formatting/sample_metastasis_times_from_posterior.py
@@ -8,6 +8,8 @@ from copy import deepcopy
 import dendropy
 from multiprocessing import Pool
 from collections import deque
+import csv
+import pickle as pkl
 
 def rename_tree(tree):
     tree_copy = deepcopy(tree)
@@ -23,48 +25,58 @@ def rename_tree(tree):
     
     newick = tree_copy.as_string(schema='newick', suppress_edge_lengths=False, node_label_element_separator=',')
 
-    # remove quoted node names
+    # remove quoted node names that is default in dendropy
     newick = newick.replace("\'", "")
 
+    # the transition to ete3 tree here is just by preference
+    # likely all the same subsequent analysis can be done with dendropy
     ete_tree = Tree(newick, format=3)
 
     return ete_tree
 
-def level_order_traversal(tree):
-    events = {}
-    queue = deque([tree.seed_node])
-    while queue:
-        node = queue.popleft()
-        for child in node.child_node_iter():
-            edge = f"{node.label.split('_')[-1]}_{child.label.split('_')[-1]}"
-            if edge not in events:
-                events[edge] = [child.edge_length]
+def level_order_traversal_met_events(tree, root_to_origin_height):
+
+    met_times = {}
+
+    for node in tree.traverse("levelorder"):
+        if node.is_root():
+            parent_tissue = origin_tissue
+            parent_time = origin_time
+            node_time = root_to_origin_height
+        else:
+            parent_node = node.up
+            parent_tissue = parent_node.name.split("_")[-1]
+            root = tree.get_tree_root()
+            parent_time = root_to_origin_height + root.get_distance(parent_node.name)
+            node_time = root_to_origin_height + root.get_distance(node.name)
+
+        node_tissue = node.name.split("_")[-1]
+
+        if node_tissue != parent_tissue:
+            migration = f"{parent_tissue}_{node_tissue}"
+            migration_time = (parent_time, node_time)
+            if migration not in met_times:
+                met_times[migration] = [migration_time]
             else:
-                events[edge].append(child.edge_length)
-            queue.append(child)
+                met_times[migration].append(migration_time)
+    
+    return met_times
+
+
 
 def is_ultrametric(tree):
-    root = tree.seed_node
+    root = tree.get_tree_root()
     leaf_distances = set()
-    for leaf in tree.leaf_node_iter():
-        leaf_distances.add(tree.distance_from_root(leaf))
+    for leaf in tree.iter_leaves():
+        leaf_distances.add(round(root.get_distance(leaf.name), 3))
     return len(leaf_distances) == 1
-
-def tree_height(tree):
-    root = tree.seed_node
-    max_distance = 0
-    for leaf in tree.leaf_node_iter():
-        distance = tree.distance_from_root(leaf)
-        if distance > max_distance:
-            max_distance = distance
-    return max_distance
         
 
 # user inputs
-posterior_file = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/snakemake_performance_12_31_24_uniform_50cells_50sites_data_7_24_24/beam/mS_854/chain_3.trees"
+posterior_file = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/chain_3.trees"
 origin_time = 250
-primary_tissue = "P"
-outfile = "./test_metastasis_times.csv"
+origin_tissue = "P"
+outfile = "./test_metastasis_times.pkl"
 cores=1
 
 # process beast posterior
@@ -75,19 +87,31 @@ num_discard = round(num_beast_trees * burnin_percent)
 beast_tree_list = beast_tree_list[num_discard:]
 num_beast_trees = len(beast_tree_list)
 
-tree = beast_tree_list[0]
+all_met_events = {}
 
-named_tree = rename_tree(tree)
+for tree in beast_tree_list:
 
-if not is_ultrametric(tree):
-    raise ValueError("The tree is not ultrametric")
+    # put tissue location tree annotations in the node names and convert to ete3 tree (these are personal preferences)
+    named_tree = rename_tree(tree)
 
-total_height = tree_height(tree)
-print(f"Total height from root to leaves: {total_height}")
+    # verify that the tree is ultrametric, ie. same distance from the root to all leaves
+    if not is_ultrametric(named_tree):
+        raise ValueError("The tree is not ultrametric")
 
-named_tree = rename_tree(tree)
+    # get the total tree height from root to any leaf
+    tree_height = named_tree.get_farthest_leaf()[1]
 
-timed_met_events = level_order_traversal(named_tree)
+    # calculate the time from the root to the origin, which is not output directly by BEAM so needs to be considered independently
+    root_to_origin_height = origin_time - tree_height
+
+    # traverse the tree to get the times of all metastatic envents in the migration graph implied by the tree
+    timed_met_events = level_order_traversal_met_events(named_tree, root_to_origin_height)
+
+    # record met events with the sample number in the posterior
+    all_met_events[tree.label.split(" ")[1]] = timed_met_events
+
+with open(outfile, "wb") as f:
+    pkl.dump(all_met_events, f)
 
 
 
