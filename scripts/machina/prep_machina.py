30c34cc0741cb99a17d69cfd67a9a0a7c3f3c486
StephenStaklinski <stephenstak@gmail.com>
2025-03-01 10:07:22 -0500

    Add mach2 to classification plotting for quinn et al data.

--- scripts/machina/prep_machina.py
+++ scripts/machina/prep_machina.py
@@ -1,4 +1,5 @@
-### Given a newick file with leaves labeled by name_tissue, then prep input Tree edge and leaf label files for MACHINA
+#!/usr/bin/env python3
+
 import sys
 import ete3
 import pandas as pd
@@ -8,9 +9,10 @@ output_dir = sys.argv[2]
 primary_tissue = str(sys.argv[3])
 leaf_labels_tsv = str(sys.argv[4])
 
-# leaf_labeled_tree = "machina_m8_sim_data/seed10046/T_seed10046_tissue_labeled_true_tree.nwk"
-# output_dir = "machina_m8_sim_data/seed10046/machina"
-# primary_tissue = "P"
+# leaf_labeled_tree = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/quinn_2021_lung_cancer_data_2_21_25/laml/5k/64/laml_trees_no_branch_lengths.nwk"
+# output_dir = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/quinn_2021_lung_cancer_data_2_21_25/mach2/5k/64"
+# primary_tissue = "LL"
+# leaf_labels_tsv = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/quinn_2021_lung_cancer_data_2_21_25/mach2/5k/64/leaf_tissues.tsv"
 
 input_file = leaf_labeled_tree.split("/")[-1]
 input_prefix = input_file.split(".")[0]
@@ -20,53 +22,44 @@ output_file_colors = output_dir + "/" + input_prefix + "_colors.txt"
 
 tree = ete3.Tree(leaf_labeled_tree, format=8)
 
-
-# Remove tissue labels for internal node names
+# Remove tissue labels for internal node names, if they exist
 for node in tree.traverse():
-    if node.is_leaf() or node.is_root():
-        continue
-    else:
+    if node.is_root():
+        node.name = 'root'
+    elif not node.is_leaf():
         current_name = node.name
         new_name = current_name.split("_")[0]
         node.name = new_name
 
-tree.get_tree_root().name = '0'
-
-
-leaf_label = pd.read_csv(leaf_labels_tsv, sep="\s+", names=['leaf', 'tissue'])
-
-edges = pd.DataFrame(columns = ['node1', 'node2'])
+edges = []
 
 for node in tree.traverse():
-    if node.is_leaf() == True:
-            name = node.name
-    else:
-        node_name = node.name
-        children = node.children
-        for child in children:
-            child_name = child.name
-            # if "_" in child_name:
-            #     child_name = child_name.split("_")[0]
-            edges.loc[len(edges)] = [node_name, child_name]
+    if not node.is_leaf():
+        for child in node.children:
+            edges.append((node.name, child.name))
 
+leaf_label = pd.read_csv(leaf_labels_tsv, sep="\s+", names=['leaf', 'tissue'])
 tissues = leaf_label['tissue'].unique().tolist()
 
 # Fix when primary tissue is not a leaf label, but required in coloring scheme for MACHINA to run
 if primary_tissue not in tissues:
     tissues.append(primary_tissue)
+
 i = 1
 color_map = {}
 for tissue in tissues:
     color_map[tissue] = i
     i += 1
 
+# output files
+with open(output_file_edges, "w") as file:
+    for edge in edges:
+        file.write(f'{edge[0]}\t{edge[1]}\n')
+
 leaf_label.to_csv(output_file_leaf, sep="\t", index=False, header=False)
 
-# Output files for MACHINA
-edges.to_csv(output_file_edges, sep="\t", index=False, header = False)
-
 with open(output_file_colors, "w") as file:
-    for key,value in color_map.items():
+    for key, value in color_map.items():
         file.write(f'{key}\t{value}\n')
 
 
--- scripts/plotting/plot_all_classifications.py
+++ scripts/plotting/plot_all_classifications.py
@@ -5,33 +5,57 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def load_data(file_path):
+    return pd.read_csv(file_path, index_col=0)
 
-file_path = sys.argv[1]
-outfile = sys.argv[2]
+def calculate_percentages(data, method_name):
+    percentages = data.mean().reset_index()
+    percentages.columns = ['Seeding topology', 'Value']
+    percentages['Value'] *= 100
+    percentages['method'] = method_name
+    return percentages
 
+def plot_percentages(percentages, outfile):
+    # Rename the seeding topologies
+    percentages['Seeding topology'] = percentages['Seeding topology'].replace({
+        'met_to_met': 'Met to Met',
+        'met_to_primary': 'Primary\nReseeding'
+    })
 
-# file_path = '/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/quinn_2021_lung_cancer_data_2_21_25/beam_gtr/5k/all_consensus_classifications.csv'
-# outfile = "./test.pdf"
+    # Order the data to have the bars with MACH2 first and then BEAM
+    percentages['method'] = pd.Categorical(percentages['method'], categories=['MACH2', 'BEAM'], ordered=True)
 
+    # Create a bar plot
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='Seeding topology', y='Value', hue='method', data=percentages, palette=["Blue", "Green"])
 
-data = pd.read_csv(file_path, index_col=0)
+    plt.ylim(0, 100)
+    plt.ylabel('Percent of data', fontsize=22)
+    plt.xlabel('')
+    plt.xticks(rotation=0, fontsize=22)
+    plt.yticks(fontsize=22)
+    plt.legend(title='Method', fontsize=18, title_fontsize=20, bbox_to_anchor=(1.05, 0.5), loc='upper left', borderaxespad=0., frameon=False)
 
-# Calculate the percentage of True values in each column
-percentages = data.mean() * 100
+    # Save the plot to a file
+    plt.tight_layout()
+    plt.savefig(outfile)
+    plt.close()
 
-# Create a bar plot
-plt.figure(figsize=(10, 6))
-sns.barplot(x=percentages.index, y=percentages.values, palette="Greys")
+def main():
+    file_path = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/quinn_2021_lung_cancer_data_2_21_25/beam_gtr/5k/all_consensus_classifications.csv"
+    file_path2 = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/quinn_2021_lung_cancer_data_2_21_25/mach2/5k/all_consensus_classifications.csv"
+    outfile = "/grid/siepel/home_norepl/staklins/bayesian_phylogenetic_metastasis/results/quinn_2021_lung_cancer_data_2_21_25/beam_gtr/5k/all_consensus_classifications.pdf"
 
-plt.ylim(0,100)
+    data = load_data(file_path)
+    data2 = load_data(file_path2)
 
-plt.xlabel('Seeding topology', fontsize=22)
-plt.ylabel('Percent of data', fontsize=22)
-plt.xticks(rotation=0, fontsize=22)
-plt.yticks(fontsize=22)
+    percentages1 = calculate_percentages(data, 'BEAM')
+    percentages2 = calculate_percentages(data2, 'MACH2')
 
-# Save the plot to a file
-plt.tight_layout()
-plt.savefig(outfile)
-plt.close()
+    # Combine the percentages dataframes
+    percentages = pd.concat([percentages1, percentages2])
 
+    plot_percentages(percentages, outfile)
+
+if __name__ == "__main__":
+    main()
