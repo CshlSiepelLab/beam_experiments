80320cfa18140b510496f712368567915d57ea8b
StephenStaklinski <stephenstak@gmail.com>
2025-04-07 14:56:00 -0400

    Update to plot consensus graphs in beam_visualization within convert to consensus call

--- scripts/formatting/convert_tree_posterior_to_consensus_posterior_prob_graph.py
+++ scripts/formatting/convert_tree_posterior_to_consensus_posterior_prob_graph.py
@@ -10,6 +10,10 @@ beam_log = sys.argv[2]
 primary_tissue = sys.argv[3]
 outfile = sys.argv[4]
 cores = int(sys.argv[5])
+output_file_probability_plot = sys.argv[6]
+thresholds = str(sys.argv[7]).split(",")
+outprefix_thresholded = sys.argv[8]
+
 
 # load in data
 results = BeamResults(
@@ -24,3 +28,13 @@ results.get_consensus_graph(
     output_file=outfile
 )
 
+# plot the probability consensus graph
+results.plot_probability_graph(
+    output_file=output_file_probability_plot,
+)
+
+# plot the probability consensus graph for several thresholds
+results.plot_thresholded_graph(
+    thresholds=thresholds,
+    output_file_prefix=outprefix_thresholded,
+)
