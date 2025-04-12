106d108ee0f53012fc3db30af495e60d906202d1
StephenStaklinski <stephenstak@gmail.com>
2025-04-12 10:23:28 -0400

    Take -D args as sed replacements due to beast command line issues with spaces

--- scripts/pipelines/snakemake_nowak_lab_data/submit.sh
+++ scripts/pipelines/snakemake_nowak_lab_data/submit.sh
@@ -3,6 +3,7 @@
 export REPO_PATH=/grid/siepel/home/staklins/bayesian_phylogenetic_metastasis/scripts/pipelines/snakemake_nowak_lab_data
 
 snakemake \
+-n \
 --latency-wait 30 \
 --use-conda \
 --use-singularity \
@@ -13,6 +14,6 @@ snakemake \
 --keep-going \
 --ignore-incomplete \
 --cores 1 \
---jobs 20000 \
+--jobs 10000 \
 --cluster-config $REPO_PATH/config/cluster.yaml \
 --cluster 'qsub -cwd -pe threads {cluster.cores} -l m_mem_free={cluster.mem} -l h_rt={cluster.runtime} -o {cluster.logout} -e {cluster.logerror}'
